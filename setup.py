4ef4b7b0ad0814065fe7e07b027a04c2bf2e4f52
Christian Franke <nobody@nowhere.ws>
2016-10-13 08:52:01 +0200

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info.major >= 3:
 
 setup(
     name="wuschl",
-    version="0.1.3",
+    version="0.2.0",
     license='MIT',
     author='Christian Franke',
     author_email='nobody@nowhere.ws',
